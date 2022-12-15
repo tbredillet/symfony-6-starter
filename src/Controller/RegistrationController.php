c9207546b90b8832f2d884e797b8fbc28219c74e
Thomas Bredillet <tbredillet@gmail.com>
2022-12-15 10:50:26 +0100

    Login
    Auteur :     Thomas Bredillet <tbredillet@gmail.com>

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,18 +4,20 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Security\AppAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserAuthenticatorInterface $userAuthenticatorInterface, AppAuthenticator $appAuthenticator): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -34,9 +36,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            $handler->authenticateUserAndHandleSuccess($user, $request, $authenticator, 'main');
-
-            return $this->redirectToRoute('app_home');
+            return $userAuthenticatorInterface->authenticateUser($user, $appAuthenticator, $request);
         }
 
         return $this->render('registration/register.html.twig', [
